0b8ff05e64469760c2565b63d0c45dcf9a6a8a1d
Ubuntu <ubuntu@ip-172-31-39-58.ap-south-1.compute.internal>
2022-03-05 14:32:45 +0000

    commit1

---
+++ sai.sh
@@ -0,0 +1,9 @@
+sudo apt update -y
+sudo apt install -y openjdk-11-jdk
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update -y
+sudo apt-get install -y jenkins
+systemctl enable jenkins
+systemctl start jenkins
+systemctl status jenkins
